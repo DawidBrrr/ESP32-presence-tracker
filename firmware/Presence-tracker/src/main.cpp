65cb5509e8406825f918189b3ba1c0027f6e544b
Dawid Brach <dbrach6@gmail.com>
2026-04-19 20:06:02 +0200

    initial firmware commit

---
+++ firmware/Presence-tracker/src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+const int SENSOR_IN_PIN = 18;
+const int SENSOR_OUT_PIN = 19;
+
+int peopleCount = 0;
+
+
+void setup() {
+  pinMode(SENSOR_IN_PIN, INPUT);
+  pinMode(SENSOR_OUT_PIN, INPUT);
+}
+
+void loop() {
+
+  
+  
+}
+
