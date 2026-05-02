ab80aff74aa017e3dbc24ae5367114bb6ad573b0
Dawid Brach <dbrach6@gmail.com>
2026-05-02 16:43:11 +0200

    backend init

---
+++ backend/backend/src/main/java/pl/presence/tracker/PresenceTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.presence.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenceTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresenceTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ backend/backend/src/test/java/pl/presence/tracker/PresenceTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.presence.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresenceTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
